b87cc9277b7cabf50cf064f8cc06f0c527bb43c9
MayronBM <bmayronantonio@gmail.com>
2023-03-17 15:21:16 -0600

    cambia master

--- src/main/java/com/flow/ramasfuncion/ramasfuncion/controlador/ControllerJosue.java
+++ src/main/java/com/flow/ramasfuncion/ramasfuncion/controlador/ControllerJosue.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerJosue {
 
+
+    @GetMapping("/josueDos")
+    public String getMensajeDos() {
+        return "MENSAJE";
+    }
     @GetMapping("/josue")
     public String getMensaje() {
         return "MENSAJE";
