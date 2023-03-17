9189c0b397e240d4be7b988e7f0013d5891b72e7
JAreyes98 <josuereyeschevy@gmail.com>
2023-03-17 14:49:42 -0600

    Inicia el proyecto

---
+++ src/main/java/com/flow/ramasfuncion/ramasfuncion/RamasfuncionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.ramasfuncion.ramasfuncion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamasfuncionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamasfuncionApplication.class, args);
+	}
+
+}
