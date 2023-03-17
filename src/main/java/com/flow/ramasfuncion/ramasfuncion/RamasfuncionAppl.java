66223d08a8871dae374f2c863d7338f40930450e
MayronBM <bmayronantonio@gmail.com>
2023-03-17 15:32:02 -0600

    cambia nombre

--- src/main/java/com/flow/ramasfuncion/ramasfuncion/RamasfuncionAppl.java
+++ src/main/java/com/flow/ramasfuncion/ramasfuncion/RamasfuncionAppl.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RamasfuncionApplication {
+public class RamasfuncionAppl {
 
 	//
 	public static void main(String[] args) {
 	//<<>>
-		SpringApplication.run(RamasfuncionApplication.class, args);
+		SpringApplication.run(RamasfuncionAppl.class, //
+				args);
 	}
 
 }
